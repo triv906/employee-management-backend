310d03038de93c0893db8d5f34312c3fa0620813
Sindhuja Trivedi <tsindhuja2005@gmail.com>
2026-07-27 23:33:31 +0530

    Sprint 1: Added Employee Registration Module using OOP

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main {
-  public static void main(String[] args){
-  Employee e1=new Employee("James",122,300000,"IT");
-  Employee e2=new Employee("Robert",125,30000,"CSE");
-  e1.displayDetails();
+  public static void main(String[] args) {
+    Employee e1 = new Employee("James", 122, 300000, "IT");
+    Employee e2 = new Employee("Robert", 125, 30000, "CSE");
+    e1.displayDetails();
 
-  System.out.println();
+    System.out.println();
 
-  e2.displayDetails();
+    e2.displayDetails();
   }
-  
+
 }
